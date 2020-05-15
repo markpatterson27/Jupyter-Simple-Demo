e3b147bc2f7b02e5cf64305b31201b190b81f3a7
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2020-05-15 13:50:07 +0100

    added instructions for vscode use

--- hello-name.py
+++ hello-name.py
@@ -1,4 +1,7 @@
 # Hello name program to demonstrate using python in vscode and/or in JupyterLabs.
+#
+# If you are in vscode, press the green 'play' icon in the upper right of the screen to run the program.
+#
 
 name = input("Please enter your name: ")
 
